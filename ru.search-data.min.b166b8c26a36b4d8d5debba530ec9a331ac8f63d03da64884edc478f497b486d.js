3c85e0113293f613f84f7325c07bc2bc1e23e19e
mung0001 <mung0001@naver.com>
2020-04-01 20:38:37 +0900

    init

--- ru.search-data.min.b166b8c26a36b4d8d5debba530ec9a331ac8f63d03da64884edc478f497b486d.js
+++
@@ -1,1 +0,0 @@
-'use strict';(function(){const indexCfg={split:/[^a-zа-яё0-9\w]/gi};indexCfg.doc={id:'id',field:['title','content'],store:['title','href'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;})();
